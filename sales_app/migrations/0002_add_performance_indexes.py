5590198b6f781741b5d9a1f280161d351cabe6be
Luka Jgerenaia <luka.jgerenaia@gmail.com>
2026-02-07 17:32:13 +0400

    Add caching and database indexes for performance

--- sales_app/migrations/0002_add_performance_indexes.py
+++ sales_app/migrations/0002_add_performance_indexes.py
@@ -3,7 +3,7 @@ from django.db import migrations
 
 
 class Migration(migrations.Migration):
-    
+    atomic = False 
     dependencies = [
         ('sales_app', '0001_initial'),  # ← Change this to your last migration
     ]
